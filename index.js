c32a64f41d960157ad017c56084c96ead00d524e
Ibaihima <ibaihima@hotmail.com>
2022-02-07 10:59:46 -0500

    commit tree 5f9c8a070dfd1d20cdb6cd0da4e10ab9cb3e2023

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+// const input = document.getElementById('input');
 
-function clickAlert() {
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
   alert('I was clicked!');
-}
+});
 
-input.addEventListener('click', clickAlert);
+const a = document.getElementById("input") 
+
+const addingEventListener = () => {
+  return input.addEventListener("click", () => {
+  alert("giggles")
+})
 }
